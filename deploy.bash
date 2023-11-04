34a5bf1e55c1a0a42087d172bfac6d115074a123
bios62 <inge.os@oracle.com>
2023-11-04 06:40:10 +0100

    rettet deploy script

---
+++ deploy.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+set python-scripts scripts
+for DDIR in $*
+do
+  DIR=/home/pi/$DDIR
+  SRC=/home/pi/ha_hytta/$DDIR
+  if [ -d $DIR ]; then echo $DIR exists; else mkdir $DIR;echo $DIR created; fi
+  rm $DIR/*
+  cp $SRC/* $DIR/.
+done
+
